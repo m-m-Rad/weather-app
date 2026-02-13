1c3f88f0a3ffc89d217b9b8b9e77a784687a6205
Rad <mamaf123.rad@gmail.com>
2026-02-13 15:21:55 +0330

    the main info html is added .service empty file is added for fetching the info

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Weather } from './weather/weather';
+import { WeatherService } from './weather/weather.service';
 
 export const routes: Routes = [
   {
     path: '',
     component: Weather,
+    providers: [WeatherService],
   },
 ];
