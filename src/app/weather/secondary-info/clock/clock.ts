b77329afdcd94d6ee39ba8dcf79566715e939674
Rad <mamaf123.rad@gmail.com>
2026-02-16 20:04:17 +0330

    clock is now reactive the position of  hands are not perfect

--- src/app/weather/secondary-info/clock/clock.ts
+++ src/app/weather/secondary-info/clock/clock.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 
 @Component({
   selector: 'app-clock',
@@ -7,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './clock.css',
 })
 export class Clock {
-
+  title = input.required<string>();
+  timeInput = input.required<string>();
+  time = computed(() => {
+    const withZero = this.timeInput().split(' ')[0];
+    return withZero.substring(1);
+  });
+  hourDeg = computed(() => {
+    return +this.time().split(':')[0] * 30;
+  });
+  minDeg = computed(() => {
+    return +this.time().split(':')[1] * 6;
+  });
 }
