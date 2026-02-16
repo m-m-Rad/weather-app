b00d98e86b94e0ab2c8440865b9b1fcc5a74ec6e
Rad <mamaf123.rad@gmail.com>
2026-02-16 20:07:53 +0330

    simple line was added and pipe was change to show only the integer

--- src/app/weather/symbol/temp.pipe.ts
+++ src/app/weather/symbol/temp.pipe.ts
@@ -6,8 +6,8 @@ import { Degree } from '../main-info/select/select.model';
 })
 export class TempPipe implements PipeTransform {
   transform(value: number, type: Degree): string {
-    if (type === 'DEFAULT' || type === 'C') return value.toString();
-    const result = ((value * 9) / 5 + 32).toFixed(1);
+    if (type === 'DEFAULT' || type === 'C') return value.toFixed(0).toString();
+    const result = ((value * 9) / 5 + 32).toFixed(0);
     return result.toString();
   }
 }
